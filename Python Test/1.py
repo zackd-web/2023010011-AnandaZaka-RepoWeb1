2dbc8835a84e2c2048fe5beb7d66e7d378e5a27c
Ananda Zaka <anandazaka23@gmail.com>
2024-11-04 09:48:12 +0700

    update

--- Python Test/1.py
+++ Python Test/1.py
@@ -1,8 +1,16 @@
+print("##############################")
+print("*****   TUGAS BAGIAN 1   *****")
+print("*****     ANANDA ZAKA 	*****")
+print("##############################")
+
 nama = input ("Masukkan Nama :")
 umur = int(input ("Masukkan Umur :")) 
-gaji = int(input ("Masukkan Gaji :"))  
+gaji = int(input ("Masukkan Gaji :"))
 
 if umur >= 21 and gaji >= 3000000:
-	print(f"Hai {nama}, Anda Sesuai dan memenuhi syarat")
+	lanjut = input(f"Hai {nama}, Anda Sesuai dan memenuhi syarat. Mau Lanjut?")
+	if lanjut == "y" or "Y":
+		print("GASS")
+		
 else:
-	print(f"Maaf {nama}, anda tidak memenuhi syarat")
+	print(f"Maaf {nama}, anda tidak memenuhi syarat. Silahkan Naikan gaji dan umur")
---
+++ Python Test/2.py
@@ -0,0 +1,11 @@
+class hewan():
+	def anjing():
+		print ("gukguk")
+	def kucing():
+		print ("meong")
+	def bebek():
+		print("kwek kwek")
+	def ayam():
+		a = input("y/n?")
+
+print(hewan.ayam())
