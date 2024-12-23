564ba642d9c6e5ef6c1904750761727a6260b9b3
Ananda Zaka <anandazaka23@gmail.com>
2024-12-23 08:18:38 +0700

    3

---
+++ JavaScript Test/Function.js
@@ -0,0 +1,10 @@
+
+	var sisiA = 8;
+	var sisiB = 3;
+	var volumeA = sisiA * sisiA * sisiA;
+	var volumeB = sisiB * sisiB * sisiB;
+
+
+	var total = volumeA + volumeB;
+
+	console.log(total);
---
+++ JavaScript Test/Function2.js
@@ -0,0 +1,20 @@
+function HelloWorld(){
+	alert("Halo Dunia");
+}
+
+function HaloCuy(){
+	alert("Halo Cuy");
+	
+}
+
+a = 10;
+b = 5;
+
+
+if (a > b) {
+	HelloWorld();
+}
+
+else {
+	HaloCuy();
+}
